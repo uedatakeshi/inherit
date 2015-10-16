da39c20437361018281d50119fbd2c9a7ffb580b
Takeshi Ueda <uedatakeshi@gmail.com>
2015-10-16 19:46:44 +0900

    add file upload

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -4,6 +4,9 @@
     window.jQuery = window.$ = require('jquery');
     require('bootstrap');
     require('jquery-ui/datepicker');
+    require('jquery.ui.widget');
+    require('jquery.iframe-transport');
+    require('jquery.fileupload');
 
     var message = 'Hello App';
     console.log(message);
@@ -21,5 +24,23 @@
     $("#myModal").on('hidden.bs.modal', function (e) {
         $(this).removeData();
     });
+
+        $('#fileupload').fileupload({
+        url: url,
+        dataType: 'json',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo('#files');
+            });
+        },
+        progressall: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            $('#progress .progress-bar').css(
+                'width',
+                progress + '%'
+            );
+        }
+    }).prop('disabled', !$.support.fileInput)
+        .parent().addClass($.support.fileInput ? undefined : 'disabled');
 
 })();
